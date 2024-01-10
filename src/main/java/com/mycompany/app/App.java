84929e508075c8cea88aeb62a923403ff9144e7d
sudhakar.satyavolu <sudhakarcgcp@outlook.com>
2024-01-10 15:46:50 +0000

    another java commit1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Devops World2!";
+    private static final String MESSAGE = "Hello Devops World3!";
 
     public App() {}
 
