cd251a5ec7f72c399ceee83c9414cc20a7733d0c
sudhakar.satyavolu <sudhakarcgcp@outlook.com>
2024-01-10 17:36:35 +0000

    project commit pipeline testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Devops World3!";
+    private static final String MESSAGE = "Hello Devops World3! Pipeline Testing";
 
     public App() {}
 
